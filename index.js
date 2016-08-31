761a381cbbfc8d4955d01d9f934e58595bf4556f
hjchoi95 <hongjoonchoi95@gmail.com>
2016-08-31 11:09:11 +0800

    changed to node.js app

---
+++ index.js
@@ -0,0 +1,31 @@
+////////////////////////////////////////////
+//      configurations
+///////////////////////////////////////////
+var express = require('express');
+var bodyParser = require("body-parser");
+var fs = require('fs');
+var app = express();
+var path = require("path");
+
+/////////////// on heroku or local
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node (heroku & local) app is running on port', app.get('port'));
+});
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+////////////////////////////////////////////
+//      page views
+///////////////////////////////////////////
+app.get('/', function(request, res) {
+  console.log("get /");
+  res.sendFile(path.join(__dirname+'/views/angularSample.html'));
+});
